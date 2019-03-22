969ef9a567b849a5d86b3d22c4f3e7863ddd114e
zhangxinfancy <48443876+zhangxinfancy@users.noreply.github.com>
2019-03-22 21:43:32 +0800

    3-22

--- 01code/ncms/src/main/java/com/newer/ncms/mapper/TeacherMapper.java
+++ 01code/ncms/src/main/java/com/newer/ncms/mapper/TeacherMapper.java
@@ -2,6 +2,7 @@ package com.newer.ncms.mapper;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
@@ -53,6 +54,24 @@ public interface TeacherMapper {
 			@Result(property = "jobsal", column = "jobsal", javaType = Double.class),
 			@Result(property = "job", column = "job", javaType = String.class) })
 	List<Student> students(HashMap<String, Object> params);
+	
+	/**
+	 *查询学生信息 带条件查询的
+	 * @param params
+	 * @return
+	 */
+	List<Student> queryStudent(HashMap<String, Object> params);
+	
+	
+	/**
+	 * .带查询条件的，查询总记录数
+	 * @param params
+	 * @return
+	 */
+	int getTotalRecordNo(HashMap<String, Object> params);
+	
+	
+	
 
 	/**
 	 * 查询所有学生的总数
